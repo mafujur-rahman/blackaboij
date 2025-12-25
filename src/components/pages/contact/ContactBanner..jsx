985e645ac58e487cb4afba427ee25c87171496c6
mahfuj926 <mahfujrahman.jewel@gmail.com>
2025-12-25 20:43:27 +0600

    Added map showing

--- src/components/pages/contact/ContactBanner..jsx
+++ src/components/pages/contact/ContactBanner..jsx
@@ -1,14 +1,25 @@
-"use client"
-import AnimatedButton from '@/components/utils/AnimatedButton';
-import Image from 'next/image';
+"use client";
+
+import AnimatedButton from "@/components/utils/AnimatedButton";
+import Image from "next/image";
 
 const ContactBanner = () => {
+  const address =
+    "20 Allée des Piboules résidence les Belenos 13800 Istres";
+
+  const openGoogleMap = () => {
+    const mapUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+      address
+    )}`;
+    window.open(mapUrl, "_blank");
+  };
+
   return (
     <div className="relative w-full h-96 md:h-125 lg:h-140 xl:h-180 overflow-hidden flex items-center justify-center">
 
       {/* Background Image */}
       <Image
-        src='/images/store.png'
+        src="/images/store.png"
         alt="Our Shopping Mall"
         fill
         priority
@@ -20,15 +31,16 @@ const ContactBanner = () => {
 
       {/* Content */}
       <div className="relative z-10 text-center text-white p-4">
-
-        {/* Main Headline */}
         <h1 className="text-2xl md:text-4xl xl:text-[80px] font-normal mb-6">
           Our Shopping Mall
         </h1>
 
-        {/* Call-to-Action Button */}
-        <AnimatedButton variant="black">Find Our Stores</AnimatedButton>
-
+        {/* CTA Button */}
+        <div onClick={openGoogleMap}>
+          <AnimatedButton variant="black">
+            Find Our Stores
+          </AnimatedButton>
+        </div>
       </div>
     </div>
   );
