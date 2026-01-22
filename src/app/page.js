b95d5ae53936233d25e1b4cd0b156d034b588379
mafujur-rahman <mdmafuj000@gmail.com>
2026-01-22 16:34:06 +0600

    trying to fix the google showing result

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-"use client"
 import Available from "@/components/pages/home/Available";
 import Banner from "@/components/pages/home/Banner";
 import FridaySale from "@/components/pages/home/FridaySale";
@@ -6,6 +5,11 @@ import HotSale from "@/components/pages/home/HotSale";
 import NewArrivals from "@/components/pages/home/NewArrivals";
 import Newsletter from "@/components/pages/home/NewsLater";
 
+export const metadata = {
+  title: "Blackaboij | Premium Fashion & Lifestyle Store",
+  description:
+    "Blackaboij is a premium fashion and lifestyle brand offering modern clothing and accessories.",
+};
 
 export default function Home() {
   return (
